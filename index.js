387f8c354ff3b279a4d17de62a9d7393ae2278d9
polygonnedpotato <cuteiethecorgi@gmail.com>
2021-05-11 22:54:50 -0700

    stuff

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-console.log('loading system mods.')
+var fs = require('fs')
+function log(content){
+  fs.appendFile('./log/base.log')
+}
+console.log('loading system mods')
 var gsh = require('./system/gsh.js')
-console.log('system loaded. begin graphical shell init.')
+console.log('system loaded. begin graphical shell init')
 gsh.startshell()
--- system/gsh.js
+++ system/gsh.js
@@ -1,10 +1,13 @@
 var http=require('http')
 var fs=require('fs')
 exports.startshell=function(){
+    console.log('creating http server on port 8080')
     http.createServer(function (req, res) {
+        console.log('reading "./app/gsh/gui.html"')
         fs.readFile('./app/gsh/gui.html', function(err, data){
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.write(data)
             return res.end()
     })}).listen(8080)
+    console.log('gui init done')
 }
